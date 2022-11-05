0fb6c596d61b7a7512e1dac111f1a74419249d46
jumashafara <jumashafara0@gmail.com>
2022-11-05 14:02:12 +0300

    added setup tools

--- modeller/settings.py
+++ modeller/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'render.apps.RenderConfig'
+    'render.apps.RenderConfig',
 ]
 
 MIDDLEWARE = [
